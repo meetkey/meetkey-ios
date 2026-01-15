bc36850c2e6020b760499c858e06aab2bb6b7002
jooyoung <qwnm7373@gmail.com>
2026-01-15 14:16:16 +0900

    🎉 [Chore] Init Project (#0)
    Xcode 프로젝트 초기 세팅 완료

---
+++ MeetKey/MeetKey/MeetKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetKeyApp.swift
+//  MeetKey
+//
+//  Created by qwnm7 on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct MeetKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
