8b4b08937fc77e6c5bde291612a710637c3b7a5f
oevinqu <gyqls0824@gmail.com>
2026-01-15 18:20:47 +0900

    feat: Custom MainTabView 구현

--- MeetKey/MeetKey/MeetKeyApp.swift
+++ MeetKey/MeetKey/MeetKeyApp.swift
@@ -11,8 +11,8 @@ import SwiftUI
 struct MeetKeyApp: App {
     var body: some Scene {
         WindowGroup {
-//            ContentView()
-            HybinHomeView()
+            ContentView()
+//            HybinMainTabView()
         }
     }
 }
